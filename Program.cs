27ca250f8658656e05a36f054045d9ecffc379fa
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2025-05-22 13:11:17 -0400

    feat(Program): Initialize the program

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Pastel;
+
+public class Program {
+
+    public static void Loop()
+    {
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1" + " Add a task".Pastel(ConsoleColor.DarkBlue));
+            Console.WriteLine("2" + " View tasks".Pastel(ConsoleColor.DarkBlue));
+            Console.WriteLine("3" + " Mark a task as complete".Pastel(ConsoleColor.DarkBlue));
+            Console.WriteLine("4" + " Exit".Pastel(ConsoleColor.DarkBlue));
+            string? choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    TodoList.Add();
+                    break;
+                case "2":
+                    TodoList.View();
+                    break;
+                case "3":
+                    TodoList.Complete();
+                    break;
+                case "4":
+                    running = false;
+                    End();
+                    return;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+            Console.WriteLine("======================".Pastel(ConsoleColor.Blue));
+        }
+    }
+
+    public static void End()
+    {
+            Console.WriteLine("======================".Pastel(ConsoleColor.DarkGreen));
+            Console.WriteLine("Goodbye!".Pastel(ConsoleColor.Blue));
+            Console.WriteLine("======================".Pastel(ConsoleColor.DarkGreen));
+    }
+
+    public static void Greet()
+    {
+        Console.WriteLine("======================".Pastel(ConsoleColor.DarkGreen));
+        Console.WriteLine("Welcome to CSharp List".Pastel(ConsoleColor.Blue));
+        Console.WriteLine("======================".Pastel(ConsoleColor.DarkGreen));
+    }
+    public static void Main()
+    {
+        Greet();
+        Loop();
+    }
+}
