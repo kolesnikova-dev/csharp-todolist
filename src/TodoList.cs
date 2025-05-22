be7ecbfa6477475c43cca9a6c41073820c8ee4b6
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2025-05-22 15:56:44 -0400

    feat(TodoList): Add validation logic

--- src/TodoList.cs
+++ src/TodoList.cs
@@ -11,15 +11,15 @@ public class TodoList()
         View();
         Console.WriteLine("Enter number of the task that you would like to complete");
         string determination = Console.ReadLine() ?? "";
-        if (int.TryParse(determination, out int taskNumber))
+        if (int.TryParse(determination, out int taskNumber) && ValidateTaskNumber(taskNumber))
         {
-            tasks[taskNumber-1] += " - ".Pastel(ConsoleColor.DarkGreen) + "completed".Pastel(ConsoleColor.White);
+            tasks[taskNumber - 1] += " - ".Pastel(ConsoleColor.DarkGreen) + "completed".Pastel(ConsoleColor.White);
         }
         else
         {
             Console.WriteLine("Invalid number");
             Complete();
-        }  
+        }
     }
 
     public static void Add()
@@ -34,7 +34,12 @@ public class TodoList()
         Console.WriteLine("======================".Pastel(ConsoleColor.Blue));
         for (int i = 0; i < tasks.Length; i++)
         {
-            Console.WriteLine(i+1 + " " + tasks[i].Pastel(ConsoleColor.Blue));
+            Console.WriteLine(i + 1 + " " + tasks[i].Pastel(ConsoleColor.Blue));
         }
+    }
+
+    public static bool ValidateTaskNumber(int taskNumber)
+    {
+       return taskNumber - 1 >= 0 && taskNumber - 1 < tasks.Length;
     }
 }
